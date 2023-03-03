3a74bc42cb7badb074da1ed9de49cf3c1ddec0ef
Kobe Dehandschutter <kobe.dehandschutter@student.hogent.be>
2023-03-03 10:53:20 +0100

    sessions toegevoegd

--- src/pages/all.tsx
+++ src/pages/all.tsx
@@ -4,8 +4,10 @@ import FAB from "~/navigation/FAB";
 import NavBar from "~/navigation/NavBar";
 import NavButtons from "~/navigation/NavButtons";
 import { GrAdd } from 'react-icons/gr';
+import Session from "~/components/Session";
 
 const All: NextPage = () => {
+  const sessions = ["Today", 1, 2, "Yesterday", 3, 4, 5, 6, "26/02/2023", 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
   return (
     <>
       <Head>
@@ -19,9 +21,13 @@ const All: NextPage = () => {
         }
       >
         <main className="flex flex-col items-center justify-center overflow-y-scroll h-full">
-            All
+          <div className="flex flex-wrap gap-8 h-full justify-center p-5">
+            {sessions.map((x) => (
+              typeof (x) == 'string' ? <h1 className="justify-center w-full text-center text-3xl underline" key={x}>{x}</h1> : <Session key={x} />
+            ))}
+          </div>
         </main>
-        <FAB text={"Create Kudo"} icon={<GrAdd />}/>
+        <FAB text={"Create Kudo"} icon={<GrAdd />} />
       </NavBar>
     </>
   );
--- src/pages/out.tsx
+++ src/pages/out.tsx
@@ -4,8 +4,10 @@ import FAB from "~/navigation/FAB";
 import NavBar from "~/navigation/NavBar";
 import NavButtons from "~/navigation/NavButtons";
 import { GrAdd } from 'react-icons/gr';
+import Kudo from "~/components/Kudo";
 
 const Out: NextPage = () => {
+  const kudos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
   return (
     <>
       <Head>
@@ -19,9 +21,13 @@ const Out: NextPage = () => {
         }
       >
         <main className="flex flex-col items-center justify-center overflow-y-scroll h-full">
-            Out
+          <div className="flex flex-wrap gap-5 h-full justify-center p-5">
+            {kudos.map((x) => (
+              <Kudo key={x} />
+            ))}
+          </div>
         </main>
-        <FAB text={"Create Kudo"} icon={<GrAdd />}/>
+        <FAB text={"Create Kudo"} icon={<GrAdd />} />
       </NavBar>
     </>
   );

05a765d689b0aafa5b5e1dc724d5175103234f9c
JonaDeNeve <jona.deneve@student.hogent.be>
2023-03-07 16:02:13 +0100

    kudo header

--- src/pages/create/editor.tsx
+++ src/pages/create/editor.tsx
@@ -12,7 +12,14 @@ const Editor: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex flex-col items-center justify-center overflow-y-scroll h-full">
+        <div className="aspect-[3/2] bg-green-400 w-full max-w-5xl">
+          <div className="kudo-header-container flex h-1/4 bg-red-500 items-center justify-center">
+            <h1 className="kudo-header">Bedankt</h1>
+          </div>
+          <canvas id="canvas" className="bg-white w-full h-3/4">
 
+          </canvas>
+        </div>
       </main>
       <FAB text={"Send"} icon={<FiSend />} url="/out" />
     </>

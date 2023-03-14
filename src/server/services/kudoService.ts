80da5536d4b77e50d471f89d4565b3acc630b703
Kobe Dehandschutter <kobe.dehandschutter@student.hogent.be>
2023-03-14 11:03:07 +0100

    userId toegevoegd in databank

--- src/server/services/kudoService.ts
+++ src/server/services/kudoService.ts
@@ -2,27 +2,28 @@ import { type Kudo } from "@prisma/client";
 import { prisma } from "../db";
 
 type Image = {
-    id: string
-    dataUrl: string
+  id: string
+  dataUrl: string
 }
 
-export const createKudo = async (dataUrl: string): Promise<Kudo> => {
-    const image: Image = await createKudoImage(dataUrl)
-    const kudo = (await prisma.kudo.create({
-      data: {
-        image: image.id,
-        liked: false,
-        comment: ''
-      },
-    }));
-    return kudo;
+export const createKudo = async (dataUrl: string, userId: string): Promise<Kudo> => {
+  const image: Image = await createKudoImage(dataUrl)
+  const kudo = (await prisma.kudo.create({
+    data: {
+      image: image.id,
+      liked: false,
+      comment: '',
+      userId: userId
+    },
+  }));
+  return kudo;
 }
 
 export const createKudoImage = async (dataUrl: string): Promise<Image> => {
   const image: Image = (await prisma.image.create({
-      data: {
-        dataUrl: dataUrl,
-      },
-    })) as Image;
+    data: {
+      dataUrl: dataUrl,
+    },
+  })) as Image;
   return image;
 }

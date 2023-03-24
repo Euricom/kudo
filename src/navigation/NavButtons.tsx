4527c172bee8e652634d47d9408bb2306792519e
Kobe Dehandschutter <kobe.dehandschutter@student.hogent.be>
2023-03-24 16:07:38 +0100

    begin navbar

--- src/navigation/NavButtons.tsx
+++ src/navigation/NavButtons.tsx
@@ -3,14 +3,20 @@ import { useRouter } from 'next/router'
 
 const NavButtons = () => {
   const router = useRouter()
+  const path = router.asPath
+  console.log(path);
+   
 
   return (
     <>
-      <div className="btn-group" data-cy='NavButtons'>
-        <Link className={"btn btn-secondary " + (router.pathname == "/" ? "btn-active" : "")} href="/" data-cy='In'>In</Link>
-        <Link className={"btn btn-secondary " + (router.pathname == "/out" ? "btn-active" : "")} href="/out" data-cy='Out'>Out</Link>
-        <Link className={"btn btn-secondary " + (router.pathname == "/all" ? "btn-active" : "")} href="/all" data-cy='All'>All</Link>
-      </div>
+        <div className="dropdown dropdown-bottom mx-auto" data-cy='NavButtons'>
+          <label tabIndex={0} className=" btn m-1 text-3xl">{path=='/all'?"all": path ==="/out"?"out": "in"}</label>
+          <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52 text-black">
+            <li><Link className="btn btn-ghost hover:btn-active " href="/" data-cy='In'>In</Link></li>
+            <li><Link className="btn btn-ghost hover:btn-active" href="/out" data-cy='Out'>Out</Link></li>
+            <li><Link className="btn btn-ghost hover:btn-active" href="/all" data-cy='All'>All</Link></li>
+          </ul>
+        </div>
     </>
   );
 };

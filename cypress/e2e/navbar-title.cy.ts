2b5051d3fc7be644762f1667c7d0ed4718f83dd1
Kobe Dehandschutter <kobe.dehandschutter@student.hogent.be>
2023-03-29 15:41:42 +0200

    test toegevoegd

--- cypress/e2e/navbar-title.cy.ts
+++ cypress/e2e/navbar-title.cy.ts
@@ -1,7 +1,10 @@
+import { normalize } from 'path'
 import { Component } from 'react'
 
 describe('navbar title spec', () => {
   it('passes', () => {
+
+
     cy.login(Cypress.env('username') as string, Cypress.env('password') as string)
 
     cy.get('[data-cy=NavButtons]').should('exist')
@@ -30,27 +33,29 @@ describe('navbar title spec', () => {
     cy.get('[data-cy=NavButtons]').should('not.exist')
     cy.get('[data-cy=NavbarTitle]').contains('Editor')
 
+    cy.wait(2000)
+
     cy.get('[data-cy=FAB]').click()
     cy.get('[data-cy=Yes]').click()
     cy.get('[data-cy=NavButtons]').should('exist')
 
 
     cy.get('[data-cy=Kudo]').first().click()
-    cy.get('[data-cy=NavbarTitle]').contains('').should("exist")
+    cy.get('[data-cy=NavbarTitle]').contains('Kudo ').should("exist")
     cy.get('[data-cy=deleteButton]').click()
-    cy.get('[data-cy=Yes]').click()
     cy.get('[data-cy=NavButtons]').should('exist')
 
     cy.get('[data-cy=NavButtonsLabel]').click()
     cy.get('[data-cy=In]').click()
+
     cy.get('[data-cy=Session]').first().click()
-    cy.get('[data-cy=NavbarTitle]').contains('Session [ID]')
+    cy.get('[data-cy=NavbarTitle]').contains('Session: ')
 
     cy.get('[data-cy=BackArrow]').click()
     cy.get('[data-cy=NavButtons]').should('exist')
     cy.get('[data-cy=NavButtonsLabel]').click()
     cy.get('[data-cy=Out]').click()
     cy.get('[data-cy=Kudo]').first().click()
-    cy.get('[data-cy=NavbarTitle]').contains('Kudo [ID]')
+    cy.get('[data-cy=NavbarTitle]').contains('Kudo ')
   })
 })

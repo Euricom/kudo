628b4fd6b3cdab85973b162e0807dd2574269e5a
Kobe Dehandschutter <kobe.dehandschutter@student.hogent.be>
2023-03-29 15:41:42 +0200

    test toegevoegd

--- src/sessions/Session.tsx
+++ src/sessions/Session.tsx
@@ -15,24 +15,24 @@ const SessionCard = ({ session }: SessionProps) => {
     return (
         <>
             <Link key={session.id} className="card bg-base-100 shadow-xl w-full h-fit md:w-96" data-cy="Session" href={"/session/" + session.id.toString()} >
-                    <div className="card-body">
-                        <h2 className="card-title text-2xl">{session.title}</h2>
-                        <div className="flex w-full gap-3">
-                            <div className="avatar">
-                                <div className="w-10 rounded-full">
-                                    <Image
-                                        src={avatar}
-                                        alt="Profile picture"
-                                    />
-                                </div>
-                            </div>
-                            <div>
-                                <h3 className="">{speaker.displayName}</h3>
-                                {/*Eindstip nog toevoegen?*/}
-                                <h3 className="badge badge-primary">{new Date(session.date).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</h3>
+                <div className="card-body">
+                    <h2 className="card-title text-2xl" data-cy='SessionTitle'>{session.title}</h2>
+                    <div className="flex w-full gap-3">
+                        <div className="avatar">
+                            <div className="w-10 rounded-full">
+                                <Image
+                                    src={avatar}
+                                    alt="Profile picture"
+                                />
                             </div>
                         </div>
+                        <div>
+                            <h3 className="">{speaker.displayName}</h3>
+                            {/*Eindstip nog toevoegen?*/}
+                            <h3 className="badge badge-primary">{new Date(session.date).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</h3>
+                        </div>
                     </div>
+                </div>
             </Link>
         </>
     );

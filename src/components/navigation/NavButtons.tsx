8a4e0d85c3f05c13657964fac6f6a0e69667bdb5
Kobe Dehandschutter <kobe.dehandschutter@student.hogent.be>
2023-03-31 10:04:09 +0200

    navbar dropdown kleuren aangepast

--- src/components/navigation/NavButtons.tsx
+++ src/components/navigation/NavButtons.tsx
@@ -13,11 +13,11 @@ const NavButtons = () => {
   return (
     <>
       <div className="dropdown dropdown-bottom mx-auto bg-base-100" data-cy='NavButtons'>
-        <label data-cy='NavButtonsLabel' onClick={() => setState(true)} tabIndex={0} className="btn btn-ghost text-3xl w-full">{path == '/all' ? "all" : path === "/out" ? "out" : "in"}&nbsp;<MdArrowDropDown size={25} /></label>
-        {state ? <ul tabIndex={0} className="dropdown-content menu mx-auto justify-center p-2 gap-1 shadow rounded-box w-full  text-black">
-          <li ><Link className="btn bg-secondary-content hover:btn-active hover:text-white px-11" href="/" data-cy='In' onClick={() => setState(false)}>In</Link></li>
-          <li><Link className="btn bg-secondary-content hover:btn-active hover:text-white" href="/out" data-cy='Out' onClick={() => setState(false)}>Out</Link></li>
-          <li><Link className="btn bg-secondary-content hover:btn-active hover:text-white" href="/all" data-cy='All' onClick={() => setState(false)}>All</Link></li>
+        <label data-cy='NavButtonsLabel' onClick={() => setState(true)} tabIndex={0} className="btn btn-ghost  m-1 text-3xl w-full">{path == '/all' ? "all" : path === "/out" ? "out" : "in"}&nbsp;<MdArrowDropDown size={25} /></label>
+        {state ? <ul tabIndex={0} className="dropdown-content menu mx-auto justify-center p-2 gap-1 shadow rounded-box w-full">
+          <li ><Link className="btn bg-base-100 hover:btn-active  px-11" href="/" data-cy='In' onClick={() => setState(false)}>In</Link></li>
+          <li><Link className="btn bg-base-100 hover:btn-active " href="/out" data-cy='Out' onClick={() => setState(false)}>Out</Link></li>
+          <li><Link className="btn bg-base-100 hover:btn-active " href="/all" data-cy='All' onClick={() => setState(false)}>All</Link></li>
         </ul> : <></>}
 
       </div>

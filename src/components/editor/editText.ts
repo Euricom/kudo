a5d96e91c77e256747b3e22f0841d5debbec92f1
Kobe Dehandschutter <kobe.dehandschutter@student.hogent.be>
2023-04-06 17:21:02 +0200

    textarea fixes

--- src/components/editor/editText.ts
+++ src/components/editor/editText.ts
@@ -1,9 +1,9 @@
 import type Konva from "konva";
 import { type Vector2d } from "konva/lib/types";
 
-type Document = {
-  documentMode?: number;
-}
+// type Document = {
+//   documentMode?: number;
+// }
 
 const editText = (areaPosition: Vector2d, textNode: Konva.Text, tr: Konva.Transformer, scale: number, onChange: (text: string) => void, container?: HTMLDivElement) => {
   const textarea = createTextArea(textNode, areaPosition, tr, scale, onChange, container)
@@ -25,7 +25,7 @@ const createTextArea = (textNode: Konva.Text, areaPosition: Vector2d, tr: Konva.
   textarea.style.position = 'absolute';
   textarea.style.top = (areaPosition.y).toString() + 'px';
   textarea.style.left = (areaPosition.x).toString() + 'px';
-  textarea.style.width = ((textNode.width() - textNode.padding() * 2) * scale * textNode.scaleX()).toString() + 'px';
+  textarea.style.width = "auto"//((textNode.width() - textNode.padding() * 2) * scale * textNode.scaleX()).toString() + 'px';
   textarea.style.height =
     ((textNode.height() - textNode.padding() * 2 + 5) * scale).toString() + 'px';
 
@@ -36,6 +36,7 @@ const createTextArea = (textNode: Konva.Text, areaPosition: Vector2d, tr: Konva.
   textarea.style.overflow = 'hidden';
   textarea.style.background = 'none';
   textarea.style.outline = 'none';
+  // textarea.style.maxWidth = "100%"
   textarea.style.resize = 'none';
   textarea.style.lineHeight = textNode.lineHeight().toString();
   textarea.style.fontFamily = textNode.fontFamily();
@@ -82,8 +83,8 @@ const createTextArea = (textNode: Konva.Text, areaPosition: Vector2d, tr: Konva.
   });
 
   textarea.addEventListener('keydown', function () {
-    const absScale = textNode.getAbsoluteScale().x;
-    setTextareaWidth(textNode.width() * absScale * scale, textNode, textarea);
+    // const absScale = textNode.getAbsoluteScale().x;
+    // setTextareaWidth(textNode.width() * absScale * scale, textNode, textarea);
     textarea.style.height = 'auto';
     textarea.style.height =
       Math.min(300, textarea.scrollHeight + textNode.fontSize()).toString() + 'px';
@@ -112,26 +113,26 @@ const createTextArea = (textNode: Konva.Text, areaPosition: Vector2d, tr: Konva.
   return textarea;
 };
 
-function setTextareaWidth(newWidth: number, textNode: Konva.Text, textarea: HTMLTextAreaElement) {
-  if (!newWidth) {
-    // set width for placeholder
-    newWidth = textNode.getTextWidth() * textNode.fontSize();
-  }
-  // some extra fixes on different browsers
-  const isSafari = /^((?!chrome|android).)*safari/i.test(
-    navigator.userAgent
-  );
-  const isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
-  if (isSafari || isFirefox) {
-    newWidth = Math.ceil(newWidth);
-  }
+// function setTextareaWidth(newWidth: number, textNode: Konva.Text, textarea: HTMLTextAreaElement) {
+//   if (!newWidth) {
+//     // set width for placeholder
+//     newWidth = textNode.getTextWidth() * textNode.fontSize();
+//   }
+//   // some extra fixes on different browsers
+//   const isSafari = /^((?!chrome|android).)*safari/i.test(
+//     navigator.userAgent
+//   );
+//   const isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+//   if (isSafari || isFirefox) {
+//     newWidth = Math.ceil(newWidth);
+//   }
 
-  const isEdge = (document as Document).documentMode || /Edge/.test(navigator.userAgent);
-  if (isEdge) {
-    newWidth += 1;
-  }
-  textarea.style.width = newWidth.toString() + 'px';
-}
+//   const isEdge = (document as Document).documentMode || /Edge/.test(navigator.userAgent);
+//   if (isEdge) {
+//     newWidth += 1;
+//   }
+//   textarea.style.width = newWidth.toString() + 'px';
+// }
 
 
 export default editText;

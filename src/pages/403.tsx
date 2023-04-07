35edbf7991e81f1b173b335533b406e8150cbf95
JonaDeNeve <jona.deneve@student.hogent.be>
2023-04-07 13:18:31 +0200

    overbodige code weghalen

--- src/pages/403.tsx
+++ src/pages/403.tsx
@@ -1,25 +1,12 @@
 import { type NextPage } from "next";
-import Head from "next/head";
 import FAB from "~/components/navigation/FAB";
 import { GrAdd } from 'react-icons/gr';
 import { NavigationBarContent } from "~/components/navigation/NavBarTitle";
-import NavButtons from "~/components/navigation/NavButtons";
-import SessionList from "~/components/sessions/SessionList";
-import { api } from "~/utils/api";
-import { useSession } from "next-auth/react";
 import { UtilButtonsContent } from "~/hooks/useUtilButtons";
-import LoadingBar from "~/components/LoadingBar";
 import Link from "next/link";
 
 
 const Unauthorised: NextPage = () => {
-  const userId = useSession().data?.user.id
-
-  const sessionsQuery = api.sessions.getSessionsBySpeaker.useQuery({ id: userId ?? "error" })
-  const sessions = sessionsQuery.data
-  if (sessionsQuery.isLoading || !sessions) {
-    return <LoadingBar />;
-  }
 
   return (
     <>
@@ -30,7 +17,7 @@ const Unauthorised: NextPage = () => {
         <></>
       </UtilButtonsContent >
       <main className="flex flex-col gap-4 items-center justify-center h-full">
-        <p className="text-3xl">403 | Unauthorised</p>
+        <div className="text-3xl">403 | Unauthorised</div>
         <Link href={"/"} className="btn btn-accent">Go home</Link>
       </main>
       <FAB text={"Create Kudo"} icon={<GrAdd />} url="/create" />

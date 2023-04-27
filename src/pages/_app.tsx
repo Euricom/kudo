3ad88c13e4bed99b1c5037309aba218868dd8e94
JonaDeNeve <jona.deneve@student.hogent.be>
2023-04-27 11:23:28 +0200

    Icon toevoegen aan de app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,43 +11,52 @@ import { UtilButtonsProvider } from "~/hooks/useUtilButtons";
 import { TitleProvider } from "~/components/navigation/NavBarTitle";
 import Footer from "~/components/navigation/Footer";
 import Menu from "~/components/navigation/Menu";
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import Head from "next/head";
+import Logo from "~/../public/images/KudoAppIcon.svg";
+import { type StaticImageData } from "next/image";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Auth>
-        <TitleProvider>
-          <UtilButtonsProvider>
-            <div className="h-screen w-full bg-base-200 dark:bg-base-300">
-              <Menu>
-                <NavBar />
-                <div className="flex-auto flex-shrink-0">
-                  <Component {...pageProps} />
-                  <ToastContainer
-                    position="top-right"
-                    autoClose={3000}
-                    hideProgressBar={false}
-                    newestOnTop={false}
-                    closeOnClick
-                    rtl={false}
-                    limit={5}
-                    pauseOnFocusLoss
-                    draggable
-                    pauseOnHover
-                    className=" mt-14" />
-                </div>
-                <Footer />
-              </Menu>
-            </div>
-          </UtilButtonsProvider>
-        </TitleProvider>
-      </Auth>
-    </SessionProvider>
+    <>
+      <Head>
+        <link rel="shortcut icon" href={(Logo as StaticImageData).src} />
+      </Head>
+      <SessionProvider session={session}>
+        <Auth>
+          <TitleProvider>
+            <UtilButtonsProvider>
+              <div className="h-screen w-full bg-base-200 dark:bg-base-300">
+                <Menu>
+                  <NavBar />
+                  <div className="flex-auto flex-shrink-0">
+                    <Component {...pageProps} />
+                    <ToastContainer
+                      position="top-right"
+                      autoClose={3000}
+                      hideProgressBar={false}
+                      newestOnTop={false}
+                      closeOnClick
+                      rtl={false}
+                      limit={5}
+                      pauseOnFocusLoss
+                      draggable
+                      pauseOnHover
+                      className=" mt-14"
+                    />
+                  </div>
+                  <Footer />
+                </Menu>
+              </div>
+            </UtilButtonsProvider>
+          </TitleProvider>
+        </Auth>
+      </SessionProvider>
+    </>
   );
 };
 

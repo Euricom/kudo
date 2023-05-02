50d690c023f712322b5f276a5e96f1173af1a7b7
Kobe Dehandschutter <kobe.dehandschutter@student.hogent.be>
2023-05-02 08:09:41 +0200

    slash command proberen lokaal, lukt niet

--- src/pages/api/kudo/createKudo.ts
+++ src/pages/api/kudo/createKudo.ts
@@ -3,6 +3,10 @@ import { type NextApiRequest, type NextApiResponse } from "next";
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log("erin");
 
-  res.send("testje");
+  res.send({});
+  // res.send({
+  //   response_type: "in_channel",
+  //   text: "...",
+  // });
   res.end();
 }

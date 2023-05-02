8cbd3d988c43ff47234eb27fa73a5d859351a598
Kobe Dehandschutter <kobe.dehandschutter@student.hogent.be>
2023-05-02 09:25:57 +0200

    api call aangepast

--- src/pages/api/kudo/createKudo.ts
+++ src/pages/api/kudo/createKudo.ts
@@ -3,7 +3,6 @@ import { type NextApiRequest, type NextApiResponse } from "next";
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log("erin");
 
-  res.send({});
   res.send({
     response_type: "in_channel",
     text: "test slash Command",

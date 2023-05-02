58c586afc7a1189d682d82e9941e06001f86a630
Kobe Dehandschutter <kobe.dehandschutter@student.hogent.be>
2023-05-02 10:04:00 +0200

    error gefixt

--- src/pages/api/kudo/createKudo.ts
+++ src/pages/api/kudo/createKudo.ts
@@ -1,7 +1,11 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
 
+interface body {
+  message: string;
+}
+
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const message: string = req.body.message;
+  const message: string = (req.body as body).message;
 
   res.send({
     response_type: "in_channel",

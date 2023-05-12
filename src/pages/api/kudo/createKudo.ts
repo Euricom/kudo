b3208b721e7380acdad15507746f27a88909dec4
Kobe Dehandschutter <kobe.dehandschutter@student.hogent.be>
2023-05-12 09:10:59 +0200

    test modal

--- src/pages/api/kudo/createKudo.ts
+++ src/pages/api/kudo/createKudo.ts
@@ -3,6 +3,7 @@ import { makeSlackKudo } from "~/server/services/kudoService";
 // import { openModal } from "~/server/services/slackService";
 import { env } from "~/env.mjs";
 import { WebClient, type FilesUploadResponse } from "@slack/web-api";
+import { openModal } from "~/server/services/slackService";
 
 interface body {
   text: string;
@@ -20,33 +21,55 @@ export default async function handler(
 ) {
   const text: string = (req.body as body).text;
   const channel: string = (req.body as body).channel_id;
+  const trigger_id: string = (req.body as body).trigger_id;
 
   const slackClient: WebClient = new WebClient(env.SLACK_APP_TOKEN);
-  //Informatie over channel opvragen
-  const channelInfoResponse = await slackClient.conversations.info({
-    channel: channel,
-  });
 
-  if (!channelInfoResponse.ok) {
+  //Direct message werkt niet
+
+  if (channel.startsWith("D")) {
+    res.status(200).json({
+      success: true,
+      message:
+        "Something went wrong, you can not send kudos in a private message, Sorry!",
+    });
+  }
+
+  //info over channel opvragen
+  let channelInfoResponse;
+  try {
+    channelInfoResponse = await slackClient.conversations.info({
+      channel: channel,
+    });
+  } catch (e) {
     res.status(200).json({
       success: true,
       message: "Something went wrong, this channel was not found.",
     });
+    res.end();
+  }
+
+  if (!channelInfoResponse?.ok) {
+    res.status(200).json({
+      success: true,
+      message: "Something went wrong, this channel was not found.",
+    });
+    res.end();
   } else if (!channelInfoResponse.channel?.is_channel) {
     res.status(200).json({
       success: true,
       message:
         "Something went wrong, you can not send kudos in a private message, Sorry!",
     });
-  } else {
-    res.status(200).json({ success: true });
+    res.end();
   }
-
+  await openModal(trigger_id);
   res.status(200).json({ success: true });
+
   const base64 = await makeSlackKudo(text);
 
   //Als bot geen lid is van channel, join de channel.
-  if (!channelInfoResponse.channel?.is_member) {
+  if (!channelInfoResponse?.channel?.is_member) {
     await slackClient.conversations.join({
       channel: channel,
     });

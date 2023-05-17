92cf698944ba61917481022c593d0606921347e2
Kobe Dehandschutter <kobe.dehandschutter@student.hogent.be>
2023-05-17 17:39:35 +0200

    try different button

--- src/pages/api/kudo/createKudo.ts
+++ src/pages/api/kudo/createKudo.ts
@@ -354,19 +354,23 @@ const sendFirstModal = async (trigger_id: string) => {
             options: names,
           },
         },
-        {
-          type: "section",
-          block_id: "section-identifier",
-          accessory: {
-            type: "button",
-            text: {
-              type: "plain_text",
-              text: "Next",
-            },
-            action_id: "button-identifier",
-          },
-        },
+        // {
+        //   type: "section",
+        //   block_id: "section-identifier",
+        //   accessory: {
+        //     type: "button",
+        //     text: {
+        //       type: "plain_text",
+        //       text: "Next",
+        //     },
+        //     action_id: "button-identifier",
+        //   },
+        // },
       ],
+      submit: {
+        type: "plain_text",
+        text: "Send",
+      },
     },
   });
 };

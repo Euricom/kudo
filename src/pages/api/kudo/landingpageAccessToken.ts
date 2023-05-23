7b817a48b70764198fb35ef31221eaac18a3f8f0
Kobe Dehandschutter <kobe.dehandschutter@student.hogent.be>
2023-05-23 10:38:59 +0200

    opnieuw proberen

--- src/pages/api/kudo/landingpageAccessToken.ts
+++ src/pages/api/kudo/landingpageAccessToken.ts
@@ -1,4 +1,8 @@
-import { type OauthAccessResponse, WebClient } from "@slack/web-api";
+import {
+  type OauthAccessResponse,
+  WebClient,
+  OauthV2AccessResponse,
+} from "@slack/web-api";
 import { type NextApiRequest, type NextApiResponse } from "next";
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
@@ -20,38 +24,40 @@ export default async function handler(
   };
   console.log("dit zou het id moeten zijn");
   console.log((req.query as QueryContent).state);
-  try {
-    await slackClient.oauth.v2
-      .access(data)
-      .then((response: OauthAccessResponse) => {
-        console.log(response);
-      });
-  } catch (e) {
-    console.log(e);
-  }
   // try {
   //   await slackClient.oauth.v2
   //     .access(data)
-  //     .then(async (response: OauthAccessResponse) => {
-  //       const access_token = response.access_token;
-  //       console.log(access_token);
-
-  //       try {
-  //         await prisma.user.update({
-  //           where: {
-  //             id: "18d332af-2d5b-49e5-8c42-9168b3910f97",
-  //           },
-  //           data: {
-  //             access_token: access_token,
-  //           },
-  //         });
-  //       } catch (e) {
-  //         console.log(e);
-  //       }
+  //     .then((response: OauthV2AccessResponse) => {
+  //       console.log(response);
   //     });
   // } catch (e) {
   //   console.log(e);
   // }
+  try {
+    await slackClient.oauth.v2
+      .access(data)
+      .then(async (response: OauthV2AccessResponse) => {
+        const access_token1 = response.access_token;
+        const access_token2 = response.authed_user?.access_token;
+        console.log(access_token1);
+        console.log(access_token2);
+
+        try {
+          await prisma.user.update({
+            where: {
+              id: (req.query as QueryContent).state,
+            },
+            data: {
+              access_token: access_token1,
+            },
+          });
+        } catch (e) {
+          console.log(e);
+        }
+      });
+  } catch (e) {
+    console.log(e);
+  }
 
   console.log(data);
 
